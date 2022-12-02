2801fdf88cc24995241ddc1a85175506c6e02493
Juliakuznetsovaaaa <jyliakuz@gmail.com>
2022-12-02 19:32:56 +0300

    Добавила тэги, класс тулбар, починила ноушн ассерт, рабоет только через фул икспа

---
+++ src/main/java/com/ToolbarPage.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ToolbarPage {
+    public WebDriver driver;
+    public ToolbarPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+    @FindBy (xpath = "//*[contains(@aria-label, 'Музыка')]")
+    WebElement musicToolbar;
+
+
+}
